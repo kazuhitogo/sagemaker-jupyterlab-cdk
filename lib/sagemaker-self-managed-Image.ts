6707865a1c3bc0ca75bd2da96a7be1775b6ab3e6
Kazuhito Go <gokazu@amazon.co.jp>
2024-01-04 23:37:46 +0900

    リファクタ

--- lib/sagemaker-self-managed-Image.ts
+++ lib/sagemaker-self-managed-Image.ts
@@ -1,10 +1,10 @@
-import {Stack, StackProps, RemovalPolicy, CfnOutput} from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Repository } from 'aws-cdk-lib/aws-ecr';
-import { DockerImageAsset, Platform } from 'aws-cdk-lib/aws-ecr-assets';
-import { ECRDeployment, DockerImageName } from 'cdk-ecr-deployment';
-import {ContainerImage} from 'aws-cdk-lib/aws-ecs';
-import {resolve} from 'path'
+import { Stack, StackProps, RemovalPolicy, CfnOutput } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Repository } from "aws-cdk-lib/aws-ecr";
+import { DockerImageAsset, Platform } from "aws-cdk-lib/aws-ecr-assets";
+import { ECRDeployment, DockerImageName } from "cdk-ecr-deployment";
+import { ContainerImage } from "aws-cdk-lib/aws-ecs";
+import { resolve } from "path";
 
 interface SageMakerSelfManagedImageProps extends StackProps {
     repositoryName: string;
@@ -13,29 +13,28 @@ interface SageMakerSelfManagedImageProps extends StackProps {
 
 export class SagemakerSelfManagedImage extends Stack {
     public readonly imageUri: string;
-    constructor(scope: Construct, id: string, props: SageMakerSelfManagedImageProps) {
+    constructor(scope: Construct, id: string, props: SageMakerSelfManagedImageProps,) {
         super(scope, id, props);
-
         const repo = new Repository(this, `Repository`, {
-            repositoryName : props.repositoryName,
+            repositoryName: props.repositoryName,
             removalPolicy: RemovalPolicy.DESTROY,
-            autoDeleteImages: true
+            autoDeleteImages: true,
         });
-        
-        const image = new DockerImageAsset(this, 'CDKDockerImage', {
+
+        const image = new DockerImageAsset(this, "CDKDockerImage", {
             directory: resolve(__dirname, "..", "container"),
-            platform: Platform.LINUX_AMD64
+            platform: Platform.LINUX_AMD64,
         });
 
-        this.imageUri = ContainerImage.fromEcrRepository(repo, props.versionTag).imageName;
+        this.imageUri = ContainerImage.fromEcrRepository(repo, props.versionTag,).imageName;
 
-        new ECRDeployment(this, 'DeployDockerImage2', {
+        new ECRDeployment(this, "DeployDockerImage2", {
             src: new DockerImageName(image.imageUri),
             dest: new DockerImageName(this.imageUri),
         });
 
-        new CfnOutput(this, 'imageUri',{
-            value:this.imageUri
+        new CfnOutput(this, "imageUri", {
+            value: this.imageUri,
         });
     }
 }
